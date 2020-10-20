4deca921096cdbc0583076d91608e76cdc8e9937
u44807 <u44807@s001-n051.aidevcloud>
2020-10-20 07:07:55 -0700

    Fixing compilations scripts

--- labs/lab3/compile.sh
+++ labs/lab3/compile.sh
@@ -1,5 +1,5 @@
 /bin/echo "##" $(whoami) is working oneAPI_Toolkits_with_FPGAs Hough_transform lab WSCAD2020, dir $(pwd) 
-source /opt/intel/inteloneapi/setvars.sh
+#source /opt/intel/inteloneapi/setvars.sh
 dpcpp -fintelfpga -DFPGA_EMULATOR hough_transform.cpp -o fpga.emu
 ./fpga.emu
 dpcpp -fintelfpga -c hough_transform.cpp -o fpga.o
